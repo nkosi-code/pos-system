082c2471da1582b29004684fa788607d9f2761ba
Siphiwo <dev.nkosiii@gmail.com>
2026-04-18 00:51:07 +0200

    Added JWT

--- pos-frontend/src/pages/Welcome.js
+++ pos-frontend/src/pages/Welcome.js
@@ -7,19 +7,25 @@ function Welcome() {
             <p className="font-bold">This is V2.0.1. with major improvments.</p>
 
             <div className="mt-6 bg-gray-400 p-6 rounded shadow">
-                <p>✅ Bargrapgh was added</p>
-                <p>✅ Sidebar was added</p>
-                <p>✅ Full CRUD system</p>
-                <p>✅ Clean UI (Tailwind)</p>
-                <p>✅ Sidebar navigation</p>
-                <p>✅ Modal interactions</p>
-                <p>✅ Modal UI</p>
-                <p>✅ Login system + persistence</p>
-                <p>✅ Sales system</p>
-                <p>✅ Revenue tracking</p>
-                <p>✅ Dashboard analytics(chart)</p>
-                <p>✅ Clean Modern UI(Tailwind)</p>
-                <p>🛠️ Mojor adjustments are incoming in the next commit.</p>                
+                <p>✅ Implimented ✅</p>
+                <p>Crud Products</p>
+                <p>Auth (JWT)</p>
+                <p>Sales Logic</p>
+                
+                <p>🛠️ In Progress 🛠</p>
+                <p>Cart + Checkout</p>
+                <p>Receipts (print/PDF)</p>
+                <p>Password Hashing</p>
+                <p>Role-Based Access (admin& cashier)</p>
+                <p>Dashboard (sales analytics)</p>
+                <p>Inventory Management (stock + alerts)</p>
+                <p>Order History</p>
+                <p>Search & Filters</p>
+                <p>Payment Handling (cash/card)</p>
+                <p>Dark Mode</p>
+                <p>Error Handling + Loading States</p>
+                <p>Token Expiry + Route Protection</p>
+                <p>UI Polish (toasts, empty states)</p>
             </div>
         </div>
     );
